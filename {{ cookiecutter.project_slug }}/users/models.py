15b8c9f812c0fcfdb1d8bbc1effe4f62cd936f66
Juan Olvera <juan@jolvera.dev>
2021-12-27 19:43:37 -0600

    Add django-extension fix #2

--- {{ cookiecutter.project_slug }}/users/models.py
+++ {{ cookiecutter.project_slug }}/users/models.py
@@ -1,9 +1,10 @@
 from django.contrib.auth.models import AbstractUser
+from django_extensions.db.models import TimeStampedModel
 from django.db import models
 
 
 # Create your models here.
-class CustomUser(AbstractUser):
+class CustomUser(TimeStampedModel, AbstractUser):
     pass
 
     def __str__(self):
